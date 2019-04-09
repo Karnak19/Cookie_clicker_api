b1651711b74ad891cb88740a626f098fd14f8b09
Karnak19 <basile64.v@gmail.com>
2019-04-09 14:53:04 +0200

    created click route

--- routes/click.post.js
+++ routes/click.post.js
@@ -1,12 +1,34 @@
+const User = require("../models/user");
+const sequelize = require("sequelize");
+
 module.exports = {
    path: "/{id}/click",
    method: "POST",
    handler: (request, h) => {
-      return "/id/click bayb";
+      const payload = request.payload;
+      return User.update(
+         {
+            score: sequelize.literal("score+1")
+         },
+
+         {
+            where: {
+               id: request.params.id
+            }
+         }
+      );
    },
    options: {
-      description: "Clicking",
-      notes: "Posting a click to the user score",
-      tags: ["api"] // ADD THIS TAG
+      description: "Scores",
+      notes: "Return the collection of users scores",
+      tags: ["api"]
+      // validate: {
+      //    payload: Joi.object({
+      //       : Joi.string()
+      //          .alphanum()
+      //          .min(5)
+      //          .max(20)
+      //    })
+      // }
    }
 };
