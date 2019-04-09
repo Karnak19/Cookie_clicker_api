6da0b486d5427f79cd09d3126b7945336ec392be
Karnak19 <basile64.v@gmail.com>
2019-04-09 14:41:08 +0200

    connecting first post route (subscribe) with database

--- models/user.js
+++ models/user.js
@@ -4,8 +4,8 @@ class Users extends Model {}
 Users.init(
    {
       pseudo: { type: DataTypes.STRING, allowNull: false },
-      score: { type: DataTypes.INTEGER, allowNull: false },
-      isLogged: { type: DataTypes.BOOLEAN, allowNull: false }
+      score: { type: DataTypes.INTEGER, defaultValue: 0 },
+      isLogged: { type: DataTypes.BOOLEAN, defaultValue: false }
    },
    { sequelize: require("../db/connect") }
 );
--- routes/subscribe.post.js
+++ routes/subscribe.post.js
@@ -1,12 +1,24 @@
+const User = require("../models/user");
+const Joi = require("joi");
+
 module.exports = {
    path: "/subscribe",
    method: "POST",
    handler: (request, h) => {
-      return "/subscribe bayb";
+      const payload = request.payload;
+      return User.create(payload);
    },
    options: {
       description: "Subscribing",
       notes: "Post a pseudo then return an unique ID",
-      tags: ["api"] // ADD THIS TAG
+      tags: ["api"], // ADD THIS ,
+      validate: {
+         payload: Joi.object({
+            pseudo: Joi.string()
+               .alphanum()
+               .min(5)
+               .max(20)
+         })
+      }
    }
 };
